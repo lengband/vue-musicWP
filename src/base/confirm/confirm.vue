f51a7de7dabaf6c600bfc12009a6507cdeb5fae6
lengband <lengband@163.com>
2017-08-23 23:13:59 +0800

    playlist（part 1）

--- src/base/confirm/confirm.vue
+++ src/base/confirm/confirm.vue
@@ -1,57 +1,57 @@
 <template>
-  <transition name="confirm-fade">
-    <div class="confirm" v-show="showFlag" @click.stop>
-      <div class="confirm-wrapper">
-        <div class="confirm-content">
-          <p class="text">{{text}}</p>
-          <div class="operate">
-            <div @click="cancel" class="operate-btn left">{{cancelBtnText}}</div>
-            <div @click="confirm" class="operate-btn">{{confirmBtnText}}</div>
-          </div>
+    <transition name="confirm-fade">
+        <div class="confirm" v-show="showFlag" @click.stop>
+            <div class="confirm-wrapper">
+                <div class="confirm-content">
+                    <p class="text">{{text}}</p>
+                    <div class="operate">
+                        <div @click="cancel" class="operate-btn left">{{cancelBtnText}}</div>
+                        <div @click="confirm" class="operate-btn">{{confirmBtnText}}</div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </transition>
+    </transition>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {
+export default {
     props: {
-      text: {
-        type: String,
-        default: ''
-      },
-      confirmBtnText: {
-        type: String,
-        default: '确定'
-      },
-      cancelBtnText: {
-        type: String,
-        default: '取消'
-      }
+        text: {
+            type: String,
+            default: ''
+        },
+        confirmBtnText: {
+            type: String,
+            default: '确定'
+        },
+        cancelBtnText: {
+            type: String,
+            default: '取消'
+        }
     },
     data() {
-      return {
-        showFlag: false
-      }
+        return {
+            showFlag: false
+        }
     },
     methods: {
-      show() {
-        this.showFlag = true
-      },
-      hide() {
-        this.showFlag = false
-      },
-      cancel() {
-        this.hide()
-        this.$emit('cancel')
-      },
-      confirm() {
-        this.hide()
-        this.$emit('confirm')
-      }
+        show() {
+            this.showFlag = true
+        },
+        hide() {
+            this.showFlag = false
+        },
+        cancel() {
+            this.hide()
+            this.$emit('cancel')
+        },
+        confirm() {
+            this.hide()
+            this.$emit('confirm')
+        }
     }
-  }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
--- src/components/playlist/playlist.vue
+++ src/components/playlist/playlist.vue
@@ -1,135 +1,127 @@
 <template>
-  <transition name="list-fade">
-    <div class="playlist" @click="hide" v-show="showFlag">
-      <div class="list-wrapper" @click.stop>
-        <div class="list-header">
-          <h1 class="title">
-            <i class="icon" :class="iconMode" @click="changeMode"></i>
-            <span class="text">{{modeText}}</span>
-            <span class="clear" @click="showConfirm"><i class="icon-clear"></i></span>
-          </h1>
+    <transition name="list-fade">
+        <div class="playlist" v-show="showFlag" @click="hide">
+            <div class="list-wrapper" @click.stop>
+                <div class="list-header">
+                    <h1 class="title">
+                        <i class="icon" :class="iconMode" @click="changeMode"></i>
+                        <span class="text">{{modeText}}</span>
+                        <span class="clear" @click="showConfirm">
+                            <i class="icon-clear"></i>
+                        </span>
+                    </h1>
+                </div>
+                <Scroll class="list-content" :data="sequenceList" ref="listContent">
+                    <transition-group name="list" tag="ul">
+                        <li class="item" v-for="(item, index) in sequenceList" :key="item.id" @click="seletItem(item, index)" ref="listTtem">
+                            <i class="current" :class="getCurrentIcon(item)"></i>
+                            <span class="text">{{item.name}}</span>
+                            <span class="like">
+                                <i class="icon-not-favorite"></i>
+                            </span>
+                            <span class="delete" @click.stop="deleteOne(item)">
+                                <i class="icon-delete"></i>
+                            </span>
+                        </li>
+                    </transition-group>
+                </Scroll>
+                <div class="list-operate">
+                    <div class="add">
+                        <i class="icon-add"></i>
+                        <span class="text">添加歌曲到列表</span>
+                    </div>
+                </div>
+                <div class="list-close" @click="hide">
+                    <span>关闭</span>
+                </div>
+            </div>
+            <Confirm ref="confirm" text="是否清空播放列表" confirmBtnText="清空" @confirm="confirmClear"></Confirm>
         </div>
-        <scroll ref="listContent" :data="sequenceList" class="list-content" :refreshDelay="refreshDelay">
-          <transition-group ref="list" name="list" tag="ul">
-            <li :key="item.id" class="item" v-for="(item,index) in sequenceList"
-                @click="selectItem(item,index)">
-              <i class="current" :class="getCurrentIcon(item)"></i>
-              <span class="text">{{item.name}}</span>
-              <span @click.stop="toggleFavorite(item)" class="like">
-                <i :class="getFavoriteIcon(item)"></i>
-              </span>
-              <span @click.stop="deleteOne(item)" class="delete">
-                <i class="icon-delete"></i>
-              </span>
-            </li>
-          </transition-group>
-        </scroll>
-        <div class="list-operate">
-          <div @click="addSong" class="add">
-            <i class="icon-add"></i>
-            <span class="text">添加歌曲到队列</span>
-          </div>
-        </div>
-        <div @click="hide" class="list-close">
-          <span>关闭</span>
-        </div>
-      </div>
-      <confirm ref="confirm" @confirm="confirmClear" text="是否清空播放列表" confirmBtnText="清空"></confirm>
-      <add-song ref="addSong"></add-song>
-    </div>
-  </transition>
+    </transition>
 </template>
 
 <script type="text/ecmascript-6">
-  import {mapActions} from 'vuex'
-  import {playMode} from 'common/js/config'
-  import Scroll from 'base/scroll/scroll'
-  import Confirm from 'base/confirm/confirm'
-  import AddSong from 'components/add-song/add-song'
-  import {playerMixin} from 'common/js/mixin'
-
-  export default {
+import { playMode } from 'common/js/config'
+import Scroll from 'base/scroll/scroll'
+import Confirm from 'base/confirm/confirm'
+// import AddSong from 'components/add-song/add-song'
+import { playerMixin } from 'common/js/mixin'
+import { mapActions } from 'vuex'
+export default {
     mixins: [playerMixin],
     data() {
-      return {
-        showFlag: false,
-        refreshDelay: 120
-      }
+        return {
+            showFlag: false
+        }
     },
     computed: {
-      modeText() {
-        return this.mode === playMode.sequence ? '顺序播放' : this.mode === playMode.random ? '随机播放' : '单曲循环'
-      }
+        modeText() {
+            return this.mode === playMode.sequence ? '顺序播放' : this.mode === playMode.random ? '随机播放' : '单曲循环'
+        }
     },
     methods: {
-      show() {
-        this.showFlag = true
-        setTimeout(() => {
-          this.$refs.listContent.refresh()
-          this.scrollToCurrent(this.currentSong)
-        }, 20)
-      },
-      hide() {
-        this.showFlag = false
-      },
-      showConfirm() {
-        this.$refs.confirm.show()
-      },
-      confirmClear() {
-        this.deleteSongList()
-        this.hide()
-      },
-      getCurrentIcon(item) {
-        if (this.currentSong.id === item.id) {
-          return 'icon-play'
-        }
-        return ''
-      },
-      selectItem(item, index) {
-        if (this.mode === playMode.random) {
-          index = this.playlist.findIndex((song) => {
-            return song.id === item.id
-          })
-        }
-        this.setCurrentIndex(index)
-        this.setPlayingState(true)
-      },
-      scrollToCurrent(current) {
-        const index = this.sequenceList.findIndex((song) => {
-          return current.id === song.id
-        })
-        this.$refs.listContent.scrollToElement(this.$refs.list.$el.children[index], 300)
-      },
-      deleteOne(item) {
-        this.deleteSong(item)
-        if (!this.playlist.length) {
-          this.hide()
-        }
-      },
-      addSong() {
-        this.$refs.addSong.show()
-      },
-      ...mapActions([
-        'deleteSong',
-        'deleteSongList'
-      ])
+        getCurrentIcon(item) {
+            if (this.currentSong.id === item.id) {
+                return 'icon-play'
+            }
+            return ''
+        },
+        seletItem(item, index) {
+            console.log(this.mode)
+            if (this.mode === playMode.random) {
+                index = this.playlist.findIndex((song) => {
+                    return song.id === item.id
+                })
+            }
+            this.setCurrentIndex(index)
+            this.setPlayingState(true)
+        },
+        scrollToCurrent(current) {
+            const index = this.sequenceList.findIndex((song) => {
+                return current.id === song.id
+            })
+            this.$refs.listContent.scrollToElement(this.$refs.listTtem[index], 300)
+        },
+        show() {
+            this.showFlag = true
+            setTimeout(() => {
+                this.$refs.listContent.refresh()
+            }, 20)
+            this.scrollToCurrent(this.currentSong)
+        },
+        hide() {
+            this.showFlag = false
+        },
+        deleteOne(item) {
+            if (!this.playlist.length) {
+                this.hide()
+            }
+            this.deleteSong(item)
+        },
+        showConfirm() {
+            this.$refs.confirm.show()
+        },
+        confirmClear() {
+            this.deleteSongList()
+            this.hide()
+        },
+        ...mapActions([
+            'deleteSong', 'deleteSongList'
+        ])
     },
     watch: {
-      currentSong(newSong, oldSong) {
-        if (!this.showFlag || newSong.id === oldSong.id) {
-          return
+        currentSong(newSong, oldSong) {
+            if (!this.showFlag || newSong === oldSong) {
+                return
+            }
+            this.scrollToCurrent(newSong)
         }
-        setTimeout(() => {
-          this.scrollToCurrent(newSong)
-        }, 20)
-      }
     },
     components: {
-      Scroll,
-      Confirm,
-      AddSong
+        Scroll,
+        Confirm
     }
-  }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
