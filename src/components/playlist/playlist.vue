4b2d10dea426b8d223d718ead6f6e9c90df0ef83
lengband <lengband@163.com>
2017-08-25 10:34:19 +0800

    user-ccenter complied

--- src/components/playlist/playlist.vue
+++ src/components/playlist/playlist.vue
@@ -16,8 +16,8 @@
                         <li class="item" v-for="(item, index) in sequenceList" :key="item.id" @click="seletItem(item, index)" ref="listTtem">
                             <i class="current" :class="getCurrentIcon(item)"></i>
                             <span class="text">{{item.name}}</span>
-                            <span class="like">
-                                <i class="icon-not-favorite"></i>
+                            <span class="like" @click.stop="toggleFavorite(item)">
+                                <i :class="getFavoriteIcon(item)"></i>
                             </span>
                             <span class="delete" @click.stop="deleteOne(item)">
                                 <i class="icon-delete"></i>
